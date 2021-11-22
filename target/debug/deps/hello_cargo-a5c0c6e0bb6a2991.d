e81fc6422b9c9bde623ffadae902edc7eb1a6c53
Nick Lopez <0xnicklopez@gmail.com>
2021-11-21 21:34:32 -0700

    Cargo check

---
+++ target/debug/deps/hello_cargo-a5c0c6e0bb6a2991.d
@@ -0,0 +1,5 @@
+/Users/nicklopez/repos/hello_cargo/target/debug/deps/hello_cargo-a5c0c6e0bb6a2991.rmeta: src/main.rs
+
+/Users/nicklopez/repos/hello_cargo/target/debug/deps/hello_cargo-a5c0c6e0bb6a2991.d: src/main.rs
+
+src/main.rs:
