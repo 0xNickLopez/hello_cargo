9ae9dbbd686244fc4dda65f261f1233aa8003de1
Nick Lopez <0xnicklopez@gmail.com>
2021-11-21 20:59:19 -0700

    Added cargo

---
+++ target/debug/deps/hello_cargo-06faf4398d4eb741.d
@@ -0,0 +1,5 @@
+/Users/nicklopez/repos/hello_cargo/target/debug/deps/hello_cargo-06faf4398d4eb741: src/main.rs
+
+/Users/nicklopez/repos/hello_cargo/target/debug/deps/hello_cargo-06faf4398d4eb741.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/nicklopez/repos/hello_cargo/target/debug/hello_cargo: /Users/nicklopez/repos/hello_cargo/src/main.rs
